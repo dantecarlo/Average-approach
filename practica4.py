dcfa73bc0cb9c8e408ad6fcb8b9145466d25d874
Dante <>
2018-04-09 11:40:35 -0500

    Final commit

--- practica4.py
+++ practica4.py
@@ -2,27 +2,32 @@ import random
 import pickle
 
 
-class aprox:
+class approach:
     def __init__(self, alpha):
         self.ram = []
         self.alpha = alpha
         self.aprox_r = 0
         self.aprox_e = 0
+        # self.gen_data()
         self.load_data()
-        self.aprox_real()
+        # self.aprox_real()
         self.aprox_heuristic()
+
+    def __repr__(self):
+        return ("{}".format(self.ram))
 
     def gen_data(self):
         for x in range(0, 500):
             self.ram.append(random.randrange(101))
-        rand = open('data.txt', 'w')
+        rand = open('data.dat', 'wb')
         pickle.dump(self.ram, rand)
 
     def load_data(self):
-        data = open('data.txt', 'r')
+        data = open('data.dat', 'rb')
         self.ram = pickle.load(data)
 
     def aprox_real(self):
+        print("Approach real")
         prom = 0.0
         n = 0
         for data in self.ram:
@@ -32,10 +37,13 @@ class aprox:
             else:
                 prom = round((prom * n + data) / (n + 1), 4)
                 n = n + 1
+            print(prom)
+
         self.aprox_r = prom
         return self.aprox_r
 
     def aprox_heuristic(self):
+        # print("Approach heuc")
         prom = 0.0
         n = 0
         for data in self.ram:
@@ -44,9 +52,10 @@ class aprox:
                 n = n + 1
             else:
                 prom = round((self.alpha * prom + (1 - self.alpha) * data), 4)
+            print(prom)
 
         self.aprox_e = prom
 
 
-r = aprox(0.5)
-r.gen_data()
+r = approach(0.75)
+# print(r)
